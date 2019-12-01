ce4b436e1b95c115558acb17a4b9ffe967f39a60
Brian Risinger <brian.risinger+git@gmail.com>
2019-12-01 00:19:41 -0500

    Day 1 and a base class

---
+++ Day1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode
+{
+    class Day1
+    {
+
+        public void main()
+        {
+            string[] lines = Program.readFile(1);
+
+
+            int total = 0;
+            foreach(string line in lines)
+            {
+                int num = int.Parse(line);
+                total += num / 3 - 2;
+            }
+            System.Console.WriteLine("total = " + total);
+
+
+
+            total = 0;
+            foreach (string line in lines)
+            {
+                int num = int.Parse(line);
+
+                int fuel = num / 3 - 2;
+
+                int add = fuel;
+                while (add > 0)
+                {
+                    total += add;
+                    add = add / 3 - 2;
+                }
+
+            }
+            
+
+            
+            System.Console.WriteLine("total2 = " + total);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Day1 prog = new Day1();
+            prog.main();
+        }
+
+
+        public static string[] readFile(int num)
+        {
+            int counter = 0;
+            string line;
+            Dictionary<int, string> lines = new Dictionary<int, string>();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\TroZ\Projects\AdventOfCode\AdventOfCode\AdventOfCode\day"+num+".txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                lines.Add(counter, line);
+                System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+
+            string[] ret = new string[lines.Count];
+            for(int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = lines[i];
+            }
+            return ret;
+        }
+    }
+}
